704e9d6437af277c0a53eeb3817fe091dba85dac
Adlax <h.videlier@gmail.com>
2021-10-21 11:08:37 +0200

    Dans le chapitre 14 ca marche

---
+++ Chapitre14/PokemonManager/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PokemonsComponent } from './composants/pokemons/pokemons.component';
+import { PokemonComponent } from './composants/pokemon/pokemon.component';
+
+// locale : fr et tubes
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFR from '@angular/common/locales/fr';
+registerLocaleData(localeFR);
+
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokemonsComponent,
+    PokemonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [{provide:LOCALE_ID, useValue:'fr'}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
