38d3bdf865adced16b748bb2ed5a075c32116729
Adlax <h.videlier@gmail.com>
2021-10-26 15:45:58 +0200

    Manque le fonction recherche. Sinon ok

---
+++ Chapitre16/PokemonManager/src/app/composants/pokemons/pokemons.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemons.service';
+import { FormRecherchePokemon } from 'src/app/modeles/form-recherche-pokemon';
+import { faOptinMonster } from '@fortawesome/free-brands-svg-icons';
+
+@Component({
+  selector: 'app-pokemons',
+  templateUrl: './pokemons.component.html',
+  styleUrls: ['./pokemons.component.css']
+})
+export class PokemonsComponent implements OnInit {
+
+  // pokemons:{id:number, nom:string, img:string, categorie:string, types:string[], taille:number, poids:number}[] = [];
+  pokemons: any;
+  comptage:number = 0;
+  pokemonSelectionne:any;
+  faOptinMonster = faOptinMonster;
+  formRecherchePokemon = new FormRecherchePokemon('');
+  easterEgg = false;
+
+  constructor(private pokService: PokemonService) {
+  }
+
+  ngOnInit(): void {
+    this.pokService.getPokemons().subscribe( (data) => {
+      this.pokemons = data.results;
+      this.comptage = data.count;
+    } );
+  }
+
+  // recherchePokemon(nom:string){
+  //   this.pokemons = this.pokService.recherchePokemon(nom);
+  //   if( nom === "C3PO" ){
+  //     this.easterEgg = true;
+  //   } else {
+  //     this.easterEgg = false;
+  //   }
+  // }
+
+  annulerRecherche(){
+    this.ngOnInit();
+    this.formRecherchePokemon.setNom(''); //remettre nom a '';
+  }
+
+}
